0c65e418fcf58055157ad7325fec6f96affc47b3
Alex Danvy <alex@danvy.tv>
2015-11-17 01:27:41 +0100

    .

---
+++ src/SigfoxEventSimulator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SigfoxEventSimulator
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static bool breaking = false;
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            var data = new DeviceData();
+            data.Device = "SIMUL001";
+            var sas = "SharedAccessSignature sr=https%3a%2f%2fsigfox.servicebus.windows.net%2fsigfox%2fpublishers%2fsimul001%2fmessages&sig=N0ektKSEMjBzgyK49AKGxkneN14MiFDjvKknh49Der4%3d&se=1450191638&skn=device";
+            var serviceNamespace = "sigfox";
+            var hubName = "sigfox";
+            var url = string.Format("{0}/publishers/{1}/messages", hubName, data.Device);
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(string.Format("https://{0}.servicebus.windows.net/", serviceNamespace))
+            };
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", sas);
+            var i = 1;
+            while (!breaking)
+            {
+                data.Temp = 19 + rnd.Next(-2, 2);
+                data.Time = DateTime.Now;
+                try
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+                    var response = httpClient.PostAsync(url, content).Result;
+                    Console.WriteLine(string.Format("{0} - Temp={1}, Response={2}", i, data.Temp, response.StatusCode));
+                }
+                catch
+                {
+
+                }
+                Thread.Sleep(100);
+            }
+        }
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            breaking = true;
+        }
+    }
+}
---
+++ src/SignatureGenerator/Models/SignatureModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignatureGenerator.Models
+{
+    public class SignatureModel
+    {
+/*
+        public static TokenProvider CreateTokenProviderForSender(string senderKeyName, string senderKey, string serviceNamespace, string hubName, string publisherName, TimeSpan tokenTimeToLive)
+        {
+            return TokenProvider.CreateSharedAccessSignatureTokenProvider(CreateForSender(senderKeyName, senderKey, serviceNamespace, hubName, publisherName, tokenTimeToLive));
+        }
+
+        public static string CreateForSender(string senderKeyName, string senderKey, string serviceNamespace, string hubName, string publisherName, TimeSpan tokenTimeToLive)
+        {
+            var serviceUri = ServiceBusEnvironment.CreateServiceUri("sb", serviceNamespace, String.Format("{0}/publishers/{1}", hubName, publisherName))
+                .ToString()
+                .Trim('/');
+            return SharedAccessSignatureTokenProvider.GetSharedAccessSignature(senderKeyName, senderKey, serviceUri, tokenTimeToLive);
+        }
+
+        public static string CreateForHttpSender(string senderKeyName, string senderKey, string serviceNamespace, string hubName, string publisherName, TimeSpan tokenTimeToLive)
+        {
+            var serviceUri = ServiceBusEnvironment.CreateServiceUri("https", serviceNamespace, String.Format("{0}/publishers/{1}/messages", hubName, publisherName))
+                .ToString()
+                .Trim('/');
+            return SharedAccessSignatureTokenProvider.GetSharedAccessSignature(senderKeyName, senderKey, serviceUri, tokenTimeToLive);
+        }
+    }
+    */
+}
